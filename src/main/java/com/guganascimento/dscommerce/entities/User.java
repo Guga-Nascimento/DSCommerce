b997d89d15a85e98e94b3a720a4307b7d1b3c947
Guga-Nascimento <guganascimento001@gmail.com>
2024-02-17 18:41:16 -0300

    User role model

--- src/main/java/com/guganascimento/dscommerce/entities/User.java
+++ src/main/java/com/guganascimento/dscommerce/entities/User.java
@@ -3,9 +3,7 @@ package com.guganascimento.dscommerce.entities;
 
 import jakarta.persistence.*;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Entity
 @Table(name = "tb_user")
@@ -20,6 +18,12 @@ public class User {
     private String phone;
     private LocalDate birthDate; // data de nascimento
     private String password;
+
+    @ManyToMany
+    @JoinTable(name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
@@ -86,6 +90,17 @@ public class User {
 
     public List<Order> getOrders() {
         return orders;
+    }
+
+    public void addRole(Role role){
+        roles.add(role);
+    }
+    public boolean hasRole(String roleName){
+        for(Role role : roles){
+           if( role.getAuthority().equals(roleName)){
+           return true;}
+        }
+        return false;
     }
 
     @Override
--- target/classes/import.sql
+++ target/classes/import.sql
@@ -70,6 +70,13 @@ INSERT INTO tb_order_item (order_id, product_id, quantity, price) VALUES (3, 1, 
 INSERT INTO tb_payment (order_id, moment) VALUES (1, TIMESTAMP WITH TIME ZONE '2022-07-25T15:00:00Z');
 INSERT INTO tb_payment (order_id, moment) VALUES (2, TIMESTAMP WITH TIME ZONE '2022-07-30T11:00:00Z');
 
+INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+
 
 
 
